d142daf6377a4b455da5594b908d822836fccb82
AKSUMRUS <51446624+AKSUMRUS@users.noreply.github.com>
2020-10-31 08:05:49 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,145 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const Title = (text) => {
+  return <Text style={styles.titleD}>{text}</Text>;
 }
 
+const Content = (text) => {
+  return <Text style={styles.textD}>{text}</Text>;
+}
+
+const App = () => {
+  const [timesPressed, setTimesPressed] = useState(0);
+
+  return (
+      <View style={styles.container}>
+        <Pressable
+            onPress={() => {
+              setTimesPressed((current) => current + 1);
+            }}
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed
+                    ? 'rgb(210, 230, 255)'
+                    : 'white'
+              },
+              styles.wrapperCustom
+            ]}>
+          {({ pressed }) => (
+              <Text style={styles.text}>
+                +
+              </Text>
+          )}
+        </Pressable>
+        <Pressable
+            onPress={() => {
+              setTimesPressed((current) => current - 1);
+            }}
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed
+                    ? 'rgb(210, 230, 255)'
+                    : 'white'
+              },
+              styles.wrapperCustom
+            ]}>
+          {({ pressed }) => (
+              <Text style={styles.text}>
+                -
+              </Text>
+          )}
+        </Pressable>
+        <View style={styles.logBox}>
+          <Text>{timesPressed}</Text>
+        </View>
+      </View>
+  );
+  // <News title={'Programming'} text={'Is the best job in the world'}/>
+}
+
+
 const styles = StyleSheet.create({
+  header:{
+
+  },
+  a:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  footer:{
+
+  },
+  titleA:{
+    fontSize: 32,
+    color: 'red'
+  },
+  textA:{
+    fontSize: 20,
+    color: 'blue'
+  },
+  b:{
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  titleB:{
+    textAlign: 'center',
+    fontSize: 32,
+    color: 'red'
+  },
+  textB:{
+    textAlign: 'left',
+    fontSize: 20,
+    color: 'blue'
+  },
+  c:{
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  titleC:{
+    textAlign: 'right',
+    fontSize: 32,
+    color: 'red'
+  },
+  textC:{
+    textAlign: 'right',
+    fontSize: 20,
+    color: 'blue'
+  },
+  d:{
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  titleD:{
+    textAlign: 'center',
+    fontSize: 32,
+    color: 'red'
+  },
+  textD:{
+    textAlign: 'left',
+    fontSize: 20,
+    color: 'blue'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
+  text: {
+    fontSize: 16
+  },
+  wrapperCustom: {
+    borderRadius: 8,
+    padding: 6
+  },
+  logBox: {
+    padding: 20,
+    margin: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#f0f0f0',
+    backgroundColor: '#f9f9f9'
+  }
 });
+
+export default App;
